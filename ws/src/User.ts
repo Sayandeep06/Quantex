b0249b15ddb2f36cca81ee6d271740175ac5fec5
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-06-17 00:40:12 +0530

    Websocket logic

--- ws/src/User.ts
+++ ws/src/User.ts
@@ -1,6 +1,7 @@
 import { WebSocket } from "ws";
 import { IncomingMessage, SUBSCRIBE, UNSUBSCRIBE } from "./types/incoming";
 import { OutgoingMessage } from "./types/outgoing";
+import { SubscriptionManager } from "./SubscriptionManager";
 
 export class User{
     private id: string;
@@ -12,29 +13,30 @@ export class User{
         this.addListeners();
     }
 
-    private subscriptions: string[] = [];
+//    private subscriptions: string[] = [];
 
-    public subscribe(subscription: string){
-        this.subscriptions.push(subscription);
-    }
+//    public subscribe(subscription: string){
+//        this.subscriptions.push(subscription);
+//    }
 
-    public unsubscribe(subscription: string){
-        this.subscriptions = this.subscriptions.filter(x => x !== subscription);
-    }
+//    public unsubscribe(subscription: string){
+//        this.subscriptions = this.subscriptions.filter(x => x !== subscription);
+//    }
 
     emit(message: OutgoingMessage){
         this.ws.send(JSON.stringify(message))
     }
 
-    private addListeners(){
-        this.ws.on("message", (message: string)=>{
+    private addListeners() {
+        this.ws.on("message", (message: string) => {
             const parsedMessage: IncomingMessage = JSON.parse(message);
-            if(parsedMessage.method === SUBSCRIBE){
-                
+            if (parsedMessage.method === SUBSCRIBE) {
+                parsedMessage.params.forEach(s => SubscriptionManager.getInstance().subscribe(this.id, s));
             }
-            if(parsedMessage.method === UNSUBSCRIBE){
-                
+
+            if (parsedMessage.method === UNSUBSCRIBE) {
+                parsedMessage.params.forEach(s => SubscriptionManager.getInstance().unsubscribe(this.id, parsedMessage.params[0]));
             }
-        })
+        });
     }
 }
