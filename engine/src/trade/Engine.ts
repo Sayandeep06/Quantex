6e949c681e81812f98853cc626cea41b08e37cc2
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-06-27 02:29:10 +0530

    engine

--- engine/src/trade/Engine.ts
+++ engine/src/trade/Engine.ts
@@ -1,10 +1,10 @@
-import fs from 'fs'
-import { RedisManager } from '../RedisManager';
-import { ORDER_UPDATE, TRADE_ADDED } from '../types';
-import { CANCEL_ORDER, CREATE_ORDER, GET_DEPTH, GET_OPEN_ORDERS, MessageFromApi, ON_RAMP } from '../types/fromAPI';
-import { Fill, Order, Orderbook } from './Orderbook';
+import fs from "fs";
+import { RedisManager } from "../RedisManager";
+import { ORDER_UPDATE, TRADE_ADDED } from "../types/index";
+import { CANCEL_ORDER, CREATE_ORDER, GET_DEPTH, GET_OPEN_ORDERS, MessageFromApi, ON_RAMP } from "../types/fromApi";
+import { Fill, Order, Orderbook } from "./Orderbook";
 
-
+//TODO: Avoid floats everywhere, use a decimal similar to the PayTM project for every currency
 export const BASE_CURRENCY = "INR";
 
 interface UserBalance {
@@ -18,33 +18,147 @@ export class Engine {
     private orderbooks: Orderbook[] = [];
     private balances: Map<string, UserBalance> = new Map();
 
-
-    constructor(){
+    constructor() {
         let snapshot = null
-        try{
-            if(process.env.WITH_SNAPSHOT){
-                snapshot = fs.readFileSync("./snapshot.json")
+        try {
+            if (process.env.WITH_SNAPSHOT) {
+                snapshot = fs.readFileSync("./snapshot.json");
             }
-        }catch(e){
+        } catch (e) {
             console.log("No snapshot found");
         }
 
-        if(snapshot){
+        if (snapshot) {
             const snapshotSnapshot = JSON.parse(snapshot.toString());
-            this.orderbooks = snapshotSnapshot.orderbooks.map((o:any) => new Orderbook(o.baseAsset, o.bids, o.asks, o.lastTradeId, o.currrentPrice));
+            this.orderbooks = snapshotSnapshot.orderbooks.map((o: any) => new Orderbook(o.baseAsset, o.bids, o.asks, o.lastTradeId, o.currentPrice));
             this.balances = new Map(snapshotSnapshot.balances);
         } else {
             this.orderbooks = [new Orderbook(`TATA`, [], [], 0, 0)];
- //           this.setBaseBalances();
+            this.setBaseBalances();
         }
-
-        setInterval(() =>{
- //           this.saveSnapshot();
-        }, 1000*3)
+        setInterval(() => {
+            this.saveSnapshot();
+        }, 1000 * 3);
     }
 
-    saveSnapshot(){
-        
+    saveSnapshot() {
+        const snapshotSnapshot = {
+            orderbooks: this.orderbooks.map(o => o.getSnapshot()),
+            balances: Array.from(this.balances.entries())
+        }
+        fs.writeFileSync("./snapshot.json", JSON.stringify(snapshotSnapshot));
     }
+
+    updateBalance(userId: string, baseAsset: string, quoteAsset: string, side: "buy" | "sell", fills: Fill[], executedQty: number) {
+        if (side === "buy") {
+            fills.forEach(fill => {
+                // Update quote asset balance
+                //@ts-ignore
+                this.balances.get(fill.otherUserId)[quoteAsset].available = this.balances.get(fill.otherUserId)?.[quoteAsset].available + (fill.qty * fill.price);
+
+                //@ts-ignore
+                this.balances.get(userId)[quoteAsset].locked = this.balances.get(userId)?.[quoteAsset].locked - (fill.qty * fill.price);
+
+                // Update base asset balance
+
+                //@ts-ignore
+                this.balances.get(fill.otherUserId)[baseAsset].locked = this.balances.get(fill.otherUserId)?.[baseAsset].locked - fill.qty;
+
+                //@ts-ignore
+                this.balances.get(userId)[baseAsset].available = this.balances.get(userId)?.[baseAsset].available + fill.qty;
+
+            });
+            
+        } else {
+            fills.forEach(fill => {
+                // Update quote asset balance
+                //@ts-ignore
+                this.balances.get(fill.otherUserId)[quoteAsset].locked = this.balances.get(fill.otherUserId)?.[quoteAsset].locked - (fill.qty * fill.price);
+
+                //@ts-ignore
+                this.balances.get(userId)[quoteAsset].available = this.balances.get(userId)?.[quoteAsset].available + (fill.qty * fill.price);
+
+                // Update base asset balance
+
+                //@ts-ignore
+                this.balances.get(fill.otherUserId)[baseAsset].available = this.balances.get(fill.otherUserId)?.[baseAsset].available + fill.qty;
+
+                //@ts-ignore
+                this.balances.get(userId)[baseAsset].locked = this.balances.get(userId)?.[baseAsset].locked - (fill.qty);
+
+            });
+        }
+    }
+
+    checkAndLockFunds(baseAsset: string, quoteAsset: string, side: "buy" | "sell", userId: string, asset: string, price: string, quantity: string) {
+        if (side === "buy") {
+            if ((this.balances.get(userId)?.[quoteAsset]?.available || 0) < Number(quantity) * Number(price)) {
+                throw new Error("Insufficient funds");
+            }
+            //@ts-ignore
+            this.balances.get(userId)[quoteAsset].available = this.balances.get(userId)?.[quoteAsset].available - (Number(quantity) * Number(price));
+            
+            //@ts-ignore
+            this.balances.get(userId)[quoteAsset].locked = this.balances.get(userId)?.[quoteAsset].locked + (Number(quantity) * Number(price));
+        } else {
+            if ((this.balances.get(userId)?.[baseAsset]?.available || 0) < Number(quantity)) {
+                throw new Error("Insufficient funds");
+            }
+            //@ts-ignore
+            this.balances.get(userId)[baseAsset].available = this.balances.get(userId)?.[baseAsset].available - (Number(quantity));
+            
+            //@ts-ignore
+            this.balances.get(userId)[baseAsset].locked = this.balances.get(userId)?.[baseAsset].locked + Number(quantity);
+        }
+    }
+
+    onRamp(userId: string, amount: number) {
+        const userBalance = this.balances.get(userId);
+        if (!userBalance) {
+            this.balances.set(userId, {
+                [BASE_CURRENCY]: {
+                    available: amount,
+                    locked: 0
+                }
+            });
+        } else {
+            userBalance[BASE_CURRENCY].available += amount;
+        }
+    }
+
+    setBaseBalances() {
+        this.balances.set("1", {
+            [BASE_CURRENCY]: {
+                available: 10000000,
+                locked: 0
+            },
+            "TATA": {
+                available: 10000000,
+                locked: 0
+            }
+        });
+
+        this.balances.set("2", {
+            [BASE_CURRENCY]: {
+                available: 10000000,
+                locked: 0
+            },
+            "TATA": {
+                available: 10000000,
+                locked: 0
+            }
+        });
+
+        this.balances.set("5", {
+            [BASE_CURRENCY]: {
+                available: 10000000,
+                locked: 0
+            },
+            "TATA": {
+                available: 10000000,
+                locked: 0
+            }
+        });
+    }
+
 }
- 
